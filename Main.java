1718efdb5d697da103e0dd5dd78ce56f6ed38277
M-flores1 <mflores1@correo.um.edu.uy>
2024-06-18 11:57:49 -0300

    Elimine el scanner que sobraba y añadi la opcion de finalizar el programa!!

--- Main.java
+++ Main.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 
 //C:\\Users\\grabo\\OneDrive\\Escritorio\\Dataset2.csv
 //C:\\Users\\marti\\OneDrive\\Desktop\\2024 S1\\prog2\\universal_top_spotify_songs (1)\\universal_top_spotify_songs.csv
+
 public class Main {
     public static void main(String[] args) throws InformacionInvalida {
         Scanner scanner = new Scanner(System.in);
@@ -20,18 +21,20 @@ public class Main {
         CSV csvProcessor = new CSV();
         MyHashTableImpl<LocalDate, MyHashTableImpl<String, LinkedListImpl<Cancion>>> hashMap = csvProcessor.hashMap(direcCSV);
 
-        Scanner sc = new Scanner (System.in);
+
+
         int opcion = 0;
 
-        while (opcion != 5 ){
+        while (opcion != 6 ){
             System.out.println ("Elija una consulta para realizar:" );
             System.out.println ("1. Top 10 canciones en un dia dado");
             System.out.println ("2. Top 5 canciones con mas apariciones en los top 50 en un dia dado");
             System.out.println ("3. Top 7 artistas que mas aparecen en los top 50 para un rango de fechas");
             System.out.println ("4. Cantidad de veces que aparece un artista en un top 50");
             System.out.println ("5. Cantidad de canciones con un tempo en un rango especifico para un rango de fechas");
+            System.out.println("6. finalizar el programa.");
 
-            opcion= sc. nextInt();
+            opcion= scanner. nextInt();
 
             switch (opcion) {
                 case 1:
@@ -53,9 +56,12 @@ public class Main {
                 case 5:
                     //poner aca la funcion a la que quiero que vaya;
                     break;
+
+                case 6:
+                    System.out.println("programa finalizado.");
+                    break;
             }
         }
         scanner.close();
-        sc.close();
     }
 }
