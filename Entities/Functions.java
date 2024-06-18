bd516ede352b1e6fb560da1456f454cd0f2bbebd
GraBoragno <graboragno2005@gmail.com>
2024-06-18 14:09:47 -0300

    :))

--- Entities/Functions.java
+++ Entities/Functions.java
@@ -1,5 +1,6 @@
 package Entities;
 
+import Exceptions.InformacionInvalida;
 import TADS.Hash.MyHashTableImpl;
 import TADS.LinkedList.LinkedListImpl;
 
@@ -11,7 +12,8 @@ public class Functions {
     public Functions(MyHashTableImpl<LocalDate, MyHashTableImpl<String, LinkedListImpl<Cancion>>> hashMap) {
         this.hashMap = hashMap;
     }
-    public LinkedListImpl<Cancion> funcion2 (LocalDate fecha){
+    //abajo de esto hay que poner las funciones
+    public LinkedListImpl<Cancion> funcion2 (LocalDate fecha) throws InformacionInvalida {
         MyHashTableImpl<String, LinkedListImpl<Cancion>> hashPais = hashMap.get(fecha);
         return null;
     }
